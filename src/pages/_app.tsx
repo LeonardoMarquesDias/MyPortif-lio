de01ad651a93ec5afc51f4830d932696042c205c
LeonardoDias <leonardo_marquesdias@hotmail.com>
2021-10-01 02:25:03 +0100

    Initial | settings
    Next, Typescript, Styled-components, GlogalStyles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { AppProps } from 'next/app';
+
+import { GlobalStyle } from '../styles/globals';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default MyApp
