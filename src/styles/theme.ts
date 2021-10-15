a3d3deecac39f1d1c2ce6494c04bed98edb8646a
LeonardoDias <leonardo_marquesdias@hotmail.com>
2021-10-15 01:41:33 +0100

    add colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,15 +4,17 @@ const theme = {
   gray100: '#e1e1e6',
   gray300: '#a8a8b3',
   gray700: '#323238',
-  gray800: '#29292e',
-  gray850:' #1f2729',
+  border: '#29292e',
+  icons:' #1f2729',
   background: '#121214',
 
   green: '#04d361',
   gold: '#eba417',
   purple: '#c38cdd',
+  error: '#d45d5d',
 
   gradient: 'linear-gradient(225deg, #29292e 0%, #121214 100%);',
+  gradientreverse: 'linear-gradient(225deg, #121214 100%, #29292e 0%)'
 };
 
 export default theme;
