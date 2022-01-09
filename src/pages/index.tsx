3f70710178cc09bc6af7587ee1137a9988d520ed
LeonardoDias <leonardo_marquesdias@hotmail.com>
2022-01-09 23:45:45 +0000

    prismicData + Typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
+import Prismic from '@prismicio/client';
+import { getPrismicClient } from '../services/prismic';
+import { GetStaticProps } from 'next';
+
 import { useEffect } from 'react';
-import { HomeContainer } from '../styles/HomeStyles';
+import { HomeContainer } from '../styles/homeStyles';
 
 import Header from "../components/Header";
 import HomeHero from '../components/HomeHero';
@@ -7,13 +11,25 @@ import Experiences from '../components/Experiences'
 import MyStack from '../components/MyStack';
 import FormContact from '../components/FormContact';
 import Footer from '../components/Footer';
-
-import Aos from 'aos';
-import 'aos/dist/aos.css';
 import Repositories from '../components/Repositories';
 
+import 'aos/dist/aos.css';
+import Aos from 'aos';
 
-export default function Home() {
+interface IProjeto {
+  slug: string;
+  title: string;
+  type: string;
+  description: string;
+  github: string;
+  thumbnail: string;
+}
+
+interface HomeProps {
+  projects: IProjeto[];
+}
+
+export default function Home({ projects }: HomeProps) {
   useEffect(() => {
     Aos.init({ duration: 1500 });
   },[])
@@ -25,7 +41,7 @@ export default function Home() {
       <main className="container">
         <HomeHero />
         <Experiences />
-        <Repositories />
+        <Repositories projects={projects}/>
         <MyStack />
         <FormContact />
       </main>
@@ -33,4 +49,29 @@ export default function Home() {
       <Footer />
     </HomeContainer>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const projectResponse = await prismic.query<any>(
+    [Prismic.Predicates.at('document.type', 'projects')],
+    { orderings: '[document.first_publication_date desc]' }
+  )
+
+  const projects = projectResponse.results.map(project => ({
+    slug: project.uid,
+    title: project.data.title,
+    type: project.data.type,
+    description: project.data.description,
+    github: project.data.github.url,
+    thumbnail: project.data.thumbnail.url
+  }));
+
+  return {
+    props: {
+      projects
+    },
+    revalidate: 86400
+  };
 }
