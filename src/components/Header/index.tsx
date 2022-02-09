21cca372595d2a9f76443274f11f29752b862a2b
LeonardoDias <leonardo_marquesdias@hotmail.com>
2022-02-09 17:10:11 +0000

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,7 +11,7 @@ export default function Header() {
     <HederContent>
       <div className="container-content">
         <div>
-          <h3>Portflio</h3>
+          <h3>Portf00lio</h3>
         </div>
 
         <div>
