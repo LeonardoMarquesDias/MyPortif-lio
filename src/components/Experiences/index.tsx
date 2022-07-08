f3586a9931623f5da82aae11323d539391bbb52e
LeonardoDias <66549962+LeonardoMarquesDias@users.noreply.github.com>
2022-07-08 04:28:34 -0300

    ExperienceNewsDescription

--- src/components/Experiences/index.tsx
+++ src/components/Experiences/index.tsx
@@ -5,36 +5,37 @@ import { Container } from './styles';
 function Experiences() {
   return (
     <Container>
-      <SectionTitle  title="01 Year" description="Experience" />
+      <SectionTitle  title="02 Year" description="Experience" />
 
       <section>
         <ExperiencesItem
-          school="Rocketseat"
+          school="UI Start"
           year="2022"
-          title="ReactJS"
-          description="Lorem ipsum dolor sdsadas it amet, consectetur adipiscing elit. Aliquam"
-          certification="Certification..."
+          title="UI Design"
+          description="Basic theory and important concepts of UI Design and Figma to create interface for Mobile and Web devices"
+          certification=""
+          
         />
         <ExperiencesItem
           school="Rocketseat"
           year="2021"
-          title="Javascript"
-          description="Lorem ipsum sss dolor sit amet, consectetur adipiscing elit. Aliquam"
-          certification="Certification..."
-        />
-        <ExperiencesItem
-          school="Rocketseat"
-          year="2021"
-          title="Git"
-          description="Lorem idsadas psum dolor sit amet, consectetur adipiscing elit. Aliquam"
-          certification="Certification..."
+          title="Ignite"
+          description="Ignite is an acceleration program focusing on its expertise in ReactJS, building modern and reactive web interfaces using a modular and scalable library"
+          certification=""
         />
         <ExperiencesItem
           school="Rocketseat"
           year="2020"
-          title="CSS3 - HTML5"
-          description="Lorem ipsumdsada  dolor sit amet, consectetur adipiscing elit. Aliquam"
-          certification="Certification..."
+          title="Discover"
+          description="Discover is program focusing on web programming fundamentals and expertise in HTML5, CSS3, javascript and anothers programming solutions"
+          certification=""
+        />
+        <ExperiencesItem
+          school="Video Course"
+          year="2020"
+          title="GitHub"
+          description="Git and Github for Version Control on Git Flow methodology"
+          certification=""
         />
       </section>
     </Container>
