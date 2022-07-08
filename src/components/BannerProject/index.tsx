6b2f426ebde7ecdc783eb569000e868dd8a8795c
LeonardoDias <66549962+LeonardoMarquesDias@users.noreply.github.com>
2022-07-08 04:27:40 -0300

    BannerRemoved title, type

--- src/components/BannerProject/index.tsx
+++ src/components/BannerProject/index.tsx
@@ -1,22 +1,11 @@
 import { Container } from './styles';
 
 interface BannerProjectProps {
-  title: string;
-  type: string;
   imgUrl: string;
 }
 
-export default function BannerProject({ title, type, imgUrl}: BannerProjectProps) {
+export default function BannerProject({ imgUrl }: BannerProjectProps) {
   return (
-    <Container imgUrl={imgUrl}>
-      <div className="overlay">
-        <section>
-          <div className="container">
-            <h1>{title}</h1>
-            <h2>{type}</h2>
-          </div>
-        </section>
-      </div>
-    </Container>
+    <Container imgUrl={imgUrl} />
   );
 }
